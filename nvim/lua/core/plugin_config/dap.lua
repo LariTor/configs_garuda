755b0355bc48b10f9b1bcac6fee812da0a3e8c90
LariTor <80329463+LariTor@users.noreply.github.com>
2023-08-18 15:20:03 -0300

    Add files via upload

---
+++ nvim/lua/core/plugin_config/dap.lua
@@ -0,0 +1,22 @@
+require("dapui").setup()
+require("mason-nvim-dap").setup({
+    automatic_setup = true,
+	 handlers = {},
+})
+
+vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<F6>", ":lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<F7>", ":lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F8>", ":lua require'dap'.step_out()<CR>")
+vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>db", ":lua require'dapui'.toggle()<CR>")
+
+local dap = require('dap')
+dap.configurations.cpp[1]["args"] = function ()
+	local t = {}
+	local str = vim.fn.input('Arguments: ')
+	local _ = str:gsub("[^%s]+",function(c) table.insert(t,c) end)
+	return t
+end
+
+dap.configurations.python[1]["pythonPath"] = '/usr/bin/python'
---
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,74 @@
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("neodev").setup({
+	library = { plugins = { "nvim-dap-ui" }, types = true },
+})
+
+local on_attach = function(_, bufnr)
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+	vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+
+	vim.keymap.set('n', '<leader>fm', function() vim.lsp.buf.format { async = true } end, bufopts)
+
+	vim.api.nvim_set_keymap('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<CR>',
+		{ noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>d[', '<cmd>lua vim.diagnostic.goto_prev()<CR>',
+		{ noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>d]', '<cmd>lua vim.diagnostic.goto_next()<CR>',
+		{ noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local lspconfig = require('lspconfig')
+
+local servers = {
+	'lua_ls',
+	'pyright',
+	'rust_analyzer',
+	'arduino_language_server',
+	'clangd',
+	'julials',
+	'ltex',
+	'taplo',
+	'asm_lsp',
+	'html',
+	'cssls',
+	'tsserver',
+	'bashls',
+	'zls',
+}
+
+if not lspconfig.glslls then
+	lspconfig.glslls = {
+		default_config = {
+			cmd = { '/usr/local/bin/glslls', '--stdin' },
+			filetypes = { 'glsl', 'fs', 'vs' },
+			root_dir = function(fname)
+				return lspconfig.util.find_git_ancestor(fname)
+			end,
+			settings = {},
+		},
+	}
+end
+
+for _, lsp in ipairs(servers) do
+	lspconfig[lsp].setup {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
+end
+
+require('flutter-tools').setup {
+	lsp = {
+		on_attach = on_attach,
+	}
+}
